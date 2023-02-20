01644878b04add2e8453fd5c017f6c658c1587c7
Battler <coquin.atthaphon@outlook.com>
2023-02-20 19:31:43 +0100

    host name and picture done

--- src/pages/Accommodation.jsx
+++ src/pages/Accommodation.jsx
@@ -19,6 +19,11 @@ export default function Accommodation() {
   const accommodationTitle = getAppartmentID[0].title;
   const accommodationLocation = getAppartmentID[0].location;
   const accommodationTag = getAppartmentID[0].tags;
+
+  const hostName = getAppartmentID[0].host.name.split(" ");
+  const hostImage = getAppartmentID[0].host.picture;
+  const rating = getAppartmentID[0].rating;
+
   const description = getAppartmentID[0].description;
   const equipments = getAppartmentID[0].equipments;
 
@@ -36,9 +41,13 @@ export default function Accommodation() {
       <Carrousel imageSlider={imageSlider} />
       <main className="accommodation">
         <div className="accommodation_content">
+          {/* Title, location, tags */}
           <div className="accommodation_content_information">
+            {/* Title */}
             <h1 className="accommodation_title">{accommodationTitle}</h1>
+            {/* Location */}
             <p className="accommodation_location">{accommodationLocation}</p>
+            {/* Tags */}
             <div className="accommodation_tag">
               {accommodationTag.map((tag, index) => (
                 <p className="accommodation_tag_content" key={index}>
@@ -47,12 +56,32 @@ export default function Accommodation() {
               ))}
             </div>
           </div>
-          <div className="accommodation_content_host"></div>
+
+          {/* Host, ratings */}
+          <div className="accommodation_host">
+            <div>
+              {/* Host name */}
+              <div className="accommodation_host_name">
+                <p>{hostName[0]}</p>
+                <p>{hostName[1]}</p>
+              </div>
+
+              {/* Host picture */}
+              <img src={hostImage} alt="host" />
+            </div>
+
+            {/* Star ratings */}
+            <div className="accommodation_rating"></div>
+          </div>
         </div>
+
+        {/* Collapse components */}
         <div className="accommodation_collapse">
+          {/* Description */}
           <div className="accommodation_collapse_item">
             <Collapse title="Description" content={description} />
           </div>
+          {/* Equipment */}
           <div className="accommodation_collapse_item">
             <Collapse title="Équipements" content={equipments} />
           </div>
