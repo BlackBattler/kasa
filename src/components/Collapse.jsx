8f1caf2fa402ca38aaed617f7683fbe1444af700
Battler <coquin.atthaphon@outlook.com>
2023-02-20 21:04:25 +0100

    mobile accommodation done

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -7,18 +7,20 @@ export default function Collapse({ title, content }) {
 
   return (
     <div className="collapse">
-      <p className="collapse_title" onClick={() => setToggle(!toggle)}>
+      <h2 className="collapse_title" onClick={() => setToggle(!toggle)}>
         {title}
         <img
           className={toggle ? "arrow arrow_up" : "arrow arrow_down"}
           src={arrow}
           alt="Show content"
         />
-      </p>
+      </h2>
       <div className={toggle ? "collapse_content" : "collapse_content-hidden"}>
-        {Array.isArray(content)
-          ? content.map((item, index) => <p key={index}>{item}</p>)
-          : content}
+        {Array.isArray(content) ? (
+          content.map((item, index) => <p key={index}>{item}</p>)
+        ) : (
+          <p className="collapse_text">{content}</p>
+        )}
       </div>
     </div>
   );
